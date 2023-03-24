d90a21831164fe6aec300ab4d96ba87b99a213f0
Enes Gelmez <enes.gelmez@hotmail.com>
2023-03-24 17:21:34 +0300

    Worked on User apis

---
+++ Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestProjectAPI.Data;
+using TestProjectAPI.Models;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TestProjectAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UsersDbContext _usersDbContext;
+
+        public UserController(UsersDbContext usersDbContext)
+        {
+            this._usersDbContext = usersDbContext;
+        }
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _usersDbContext.Users.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("GetUser/{id:guid}")]
+        public async Task<IActionResult> GetUser([FromRoute] Guid id)
+        {
+            User user = await _usersDbContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("AddUsers")]
+        public async Task<IActionResult> AddUsers(AddUserRequest addUserRequest)
+        {
+
+            User user = new User()
+            {
+                Id = Guid.NewGuid(),
+                Address = addUserRequest.Address,
+                Email = addUserRequest.Email,
+                Name = addUserRequest.Name,
+                Surname = addUserRequest.Surname,
+                Phone = addUserRequest.Phone,
+                Password = EncryptPassword.HashPassword(addUserRequest.Password),
+            };
+            await _usersDbContext.Users.AddAsync(user);
+            await _usersDbContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPut]
+        [Route("UpdateUser/{id:guid}")]
+        public async Task<IActionResult> UpdateUser([FromRoute] Guid id, AddUserRequest addUserRequest)
+        {
+            User user = await _usersDbContext.Users.FindAsync(id);
+            if (user != null)
+            {
+                user.Name = addUserRequest.Name;
+                user.Surname = addUserRequest.Surname;
+                user.Phone = addUserRequest.Phone;
+                user.Email = addUserRequest.Email;
+                user.Address = addUserRequest.Address;
+                user.Password = EncryptPassword.HashPassword(addUserRequest.Password);
+                await _usersDbContext.SaveChangesAsync();
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser/{id:guid}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] Guid id)
+        {
+            User user = await _usersDbContext.Users.FindAsync(id);
+            if (user != null)
+            {
+                _usersDbContext.Remove(user);
+                await _usersDbContext.SaveChangesAsync();
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            User user = null;
+            if (!String.IsNullOrEmpty(loginRequest.Email) && !String.IsNullOrEmpty(loginRequest.Password))
+                user = await _usersDbContext.Users.FirstOrDefaultAsync(u => u.Email == loginRequest.Email);
+            else
+                return NotFound();
+            if (EncryptPassword.CheckPassword(loginRequest.Password, user.Password))
+                return Ok(true);
+            else
+                return Ok(false);
+
+        }
+    }
+}
---
+++ Data/UsersDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestProjectAPI.Models;
+
+namespace TestProjectAPI.Data
+{
+    public class UsersDbContext : DbContext
+    {
+        public UsersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
